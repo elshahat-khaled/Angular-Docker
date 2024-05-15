25824c1153236d3e58a7c9cc11979dacdd4588d6
elshahat-khaled <elshahat.khaled@iisgalvanimi.edu.it>
2024-05-15 19:16:35 +0000

    cors

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  data = {};
+  constructor(private http: HttpClient)
+  {
+    this.http.get("https://5000-elshahatkha-angulardock-zlk5kex95gn.ws-eu111.gitpod.io/simple_json")
+    .subscribe(
+      (data) => 
+      {
+        this.data = data;
+        console.log(data);
+      }
+    )
+  }
 }
--- server/app.py
+++ server/app.py
@@ -1,10 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def ping_server():
     return "Welcome to the world of animals."
+
+@app.route('https://5000-elshahatkha-angulardock-zlk5kex95gn.ws-eu111.gitpod.io/simple_json')
+def simple_json():
+    return jsonify('{saluto:ciao}')
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
