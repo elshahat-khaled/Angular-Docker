83aebcac950b1f353c27ff12f6dae732312f8af8
elshahat-khaled <elshahat.khaled@iisgalvanimi.edu.it>
2024-05-17 11:05:17 +0000

    mongodb

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,28 +11,7 @@ export class AppComponent {
   data = {};
   constructor(private http: HttpClient)
   {
-    this.http.get("https://5000-maliziag-dockergitpod-j364n3hn7ec.ws-eu107.gitpod.io/simple_json")
-    .subscribe(
-      (data) => 
-      {
-        this.data = data;
-        console.log(data);
-      }
-    )
-  }
-}
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'client';
-  data = {};
-  constructor(private http: HttpClient)
-  {
-    this.http.get("https://5000-elshahatkha-angulardock-zlk5kex95gn.ws-eu111.gitpod.io/simple_json")
+    this.http.get("https://5000-elshahatkha-angulardock-e0pdsq4lr4c.ws-eu111.gitpod.io/simple_json")
     .subscribe(
       (data) => 
       {
--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, jsonify,
+from flask import Flask, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
+import os 
 
 app = Flask(__name__)
 CORS(app)
@@ -8,9 +10,43 @@ CORS(app)
 def ping_server():
     return "Welcome to the world of animals."
 
-@app.route('https://5000-elshahatkha-angulardock-zlk5kex95gn.ws-eu111.gitpod.io/simple_json')
+@app.route('/simple_json')
 def simple_json():
     return jsonify('{saluto:ciao}')
+
+#Creo una funzione che posso riciclare ogni volta che devo accedere al DB
+def get_db():
+    client = MongoClient(host='test_mongodb',
+                         port=27017, 
+                         username='root', 
+                         password='pass',
+                        authSource="admin")
+    db = client["animal_db"]
+    return db
+
+#Creo una route per ottenere tutti gli animali
+@app.route('/animals')
+def get_stored_animals():
+    db=""
+    try:
+        db = get_db()
+        _animals = db.animal_tb.find()
+        animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
+        return jsonify({"animals": animals})
+    except:
+        pass
+    finally:
+        if type(db)==MongoClient:
+            db.close()
+
+app.route('/environment')
+def env():
+    return jsonify(
+            {"env":[
+                {"MONGO_INITDB_DATABASE": os.environ["MONGO_INITDB_DATABASE"]},
+                {"MONGO_INITDB_ROOT_USERNAME": os.environ["MONGO_INITDB_ROOT_USERNAME"]},
+                {"MONGO_INITDB_ROOT_PASSWORD": os.environ["MONGO_INITDB_ROOT_PASSWORD"]}
+            ]})
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
