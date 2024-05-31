f55c3bb1a8b97299357ccfd9fff94b73049d8534
elshahat-khaled <elshahat.khaled@iisgalvanimi.edu.it>
2024-05-31 10:59:18 +0000

    port 8080

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrlServer: 'https://5000-elshahatkha-angulardock-xgirtp3z1v0.ws-eu114.gitpod.io'
+  baseUrlServer: 'https://5000-elshahatkha-angulardock-tcs3ce95at3.ws-eu114.gitpod.io'
 };
 
 /*
